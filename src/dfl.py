60b85e801241adcd9a4d9a9bbff62a3e00bacdcf
FilipposSot <filippos.sotiropoulos@gmail.com>
2020-06-25 16:00:49 +0200

    working hybrid model

--- src/dfl.py
+++ src/dfl.py
@@ -11,6 +11,8 @@ from numpy.linalg import inv
 
 from scipy.integrate import ode
 from scipy.signal import savgol_filter
+from scipy.signal import cont2discrete
+from scipy.linalg import eigvals
 import copy
 
 import matplotlib.pyplot as plt
@@ -20,15 +22,15 @@ np.set_printoptions(suppress=True)
 
 class DFL():
     
-    def __init__(self,dynamic_plant,t_range_data = 1.0,
-                 dt_data = 0.05,n_traj_data = 300):
+    def __init__(self,dynamic_plant, t_range_data = 5.0,
+                 dt_data = 0.05, n_traj_data = 100):
         
         self.plant = dynamic_plant
         self.t_range_data = t_range_data
         self.dt_data = dt_data 
         self.n_traj_data = n_traj_data
 
-    def generate_disrete_time_system(self):
+    def generate_disrete_time_DFL_system(self):
 
         A_full = np.block([[self.plant.A_x, self.plant.A_eta],
                            [self.H_x,   self.H_eta]])
@@ -38,10 +40,9 @@ class DFL():
 
         A_disc = expm(A_full*self.dt_data)
 
-        B_disc = inv(A_full).dot(expm(A_full*self.dt_data) - np.eye(A_full.shape[0])).dot(B_full)
-
         print(A_disc)
-        print(B_disc)
+
+        B_disc = inv(A_full).dot(expm(A_full*self.dt_data) - np.eye(A_full.shape[0])).dot(B_full)
 
     def generate_data_from_random_trajectories(self):
         '''
@@ -53,9 +54,16 @@ class DFL():
         eta_data = []
         eta_dot_data = []
 
-        Y_minus_data = []
-        Y_plus_data  = []
+        
+        X_minus_data = []
         U_minus_data = []
+        Y_minus_data = []
+        Eta_minus_data = []
+
+        X_plus_data  = []
+        U_plus_data  = []
+        Y_plus_data  = []
+        Eta_plus_data  = []
 
         for i in range(self.n_traj_data):
             
@@ -65,7 +73,7 @@ class DFL():
             x_0 = np.random.uniform(self.plant.x_init_min,self.plant.x_init_max)
 
             #initialize the ode integrator
-            r = ode(self.plant.f).set_integrator('vode', method='bdf')
+            r = ode(self.plant.f).set_integrator('dopri5')
             r.set_initial_value(x_0,t_0)
 
             t_array = []
@@ -74,14 +82,20 @@ class DFL():
             eta_array = []
             y_array = []
 
+            t_control_last = -100000
+            u_t =  0.0
+
             #simulate the system
             while r.successful() and r.t < t_f:
                 
                 #define the control input. A random value is used for data generation
-                u_t =  np.random.uniform(low = -.01, high = .01)
                 r.set_f_params(u_t).set_jac_params(u_t)
-                x_t = r.integrate(r.t+self.dt_data)
-                
+                x_t = r.integrate(r.t+self.dt_data)             
+
+                if r.t - t_control_last > 1.0:
+                    t_control_last = r.t 
+                    u_t =  np.random.uniform(low = -4.0, high = 4.0)
+
                 #these are the inherent variables if the system ie input and state
                 t_array.append(r.t)
                 x_array.append(x_t)
@@ -91,22 +105,35 @@ class DFL():
                 eta_array.append(self.plant.phi(r.t,x_t,u_t))
                 y_array.append(self.plant.g(r.t,x_t,u_t))
 
-
             eta_dot_array = np.gradient(np.array(eta_array),self.dt_data)[0]
 
             eta_dot_array2 = savgol_filter(np.array(eta_array),
                                            window_length = 5, polyorder = 3,
                                            deriv = 1, axis=0)/self.dt_data
 
+            # fig, axs = plt.subplots(3, 1)
+            # axs[0].plot(np.array(t_array), np.array(x_array)[:,0], 'b')
+            # axs[1].plot(np.array(t_array), np.array(x_array)[:,1], 'r')
+            # axs[2].plot(np.array(t_array), np.array(u_array), 'g')
+
+            plt.show()
+
             Y_minus_data.append(y_array[:-1])
             U_minus_data.append(u_array[:-1])
+            X_minus_data.append(x_array[:-1])
+            Eta_minus_data.append(eta_array[:-1])
+
             Y_plus_data.append(y_array[1:])
+            U_plus_data.append(u_array[1:])
+            X_plus_data.append(x_array[1:])
+            Eta_plus_data.append(eta_array[1:])
+
 
             t_data.append(t_array)
             x_data.append(x_array)
             u_data.append(u_array)
             eta_data.append(eta_array)
-            eta_dot_data.append(eta_dot_array)
+            eta_dot_data.append(eta_dot_array2)
         
         self.t_data = np.array(t_data) 
         self.x_data = np.array(x_data)
@@ -115,25 +142,31 @@ class DFL():
         self.eta_dot_data = np.array(eta_dot_data)
 
         self.Y_minus = np.array(Y_minus_data)
-        self.Y_plus = np.array(Y_plus_data)
         self.U_minus = np.array(U_minus_data)
+        self.X_minus = np.array(X_minus_data)
+        self.Eta_minus = np.array(Eta_minus_data)
 
-    def generate_H_matrix(self,indices = None):
+        self.Y_plus = np.array(Y_plus_data)
+        self.U_plus = np.array(U_plus_data)
+        self.X_plus = np.array(X_plus_data)
+        self.Eta_plus = np.array(Eta_plus_data)
+
+    def generate_H_matrix(self):
         '''
-        regress the H matrix
+        regress the H matrix for DFL model
         '''
-        if indices is None:
 
-            Xi = np.concatenate((self.x_data.reshape(-1, self.x_data.shape[-1]),
-                                 self.eta_data.reshape(-1, self.eta_data.shape[-1]),
-                                 self.u_data.reshape(-1, self.u_data.shape[-1])),axis=1)
-            
-            Eta_dot = self.eta_dot_data.reshape(-1, self.eta_dot_data.shape[-1])
+        Xi = np.concatenate((self.x_data.reshape(-1, self.x_data.shape[-1]),
+                             self.eta_data.reshape(-1, self.eta_data.shape[-1]),
+                             self.u_data.reshape(-1, self.u_data.shape[-1])),axis=1)
+        
+        Eta_dot = self.eta_dot_data.reshape(-1, self.eta_dot_data.shape[-1])
 
-        self.H = lstsq(Xi,Eta_dot,rcond=None)[0].T
+        self.H = lstsq(np.array(Xi,dtype='float'),np.array(Eta_dot,dtype='float'),rcond=None)[0].T
         self.H_x   = self.H[:,:self.plant.N_x]
         self.H_eta = self.H[:,self.plant.N_x:self.plant.N_x+self.plant.N_eta]
         self.H_u   = self.H[:,self.plant.N_x+self.plant.N_eta:self.plant.N_x+self.plant.N_eta+self.plant.N_u]
+
 
     def generate_K_matrix(self):
 
@@ -142,218 +175,216 @@ class DFL():
         
         Y = self.Y_plus.reshape(-1, self.Y_plus.shape[-1]).T
 
-        # U_til, s_til, V_til_conj = np.linalg.svd(omega,full_matrices=False)
-        # V_til = V_til_conj.T.conj()
-
-        # U_til1 = U_til[:self.Y_plus.shape[-1], :]
-        # U_til2 = U_til[self.Y_plus.shape[-1]:, :]
-
-        # U_hat, s_hat, V_hat_conj = np.linalg.svd(Y,full_matrices=False)
-        # V_hat = V_hat_conj.T.conj()
-
-        # # _basis = Ur
-
-        # _Atilde = U_hat.T.conj().dot(Y).dot(V_til).dot(np.diag(np.reciprocal(s_til))).dot(U_til1.T.conj()).dot(U_hat)
-        # _Btilde = U_hat.T.conj().dot(Y).dot(V_til).dot(np.diag(np.reciprocal(s_til))).dot(U_til2.T.conj())
-        
-        # self.A_koop = _Atilde 
-        # self.B_koop = _Btilde 
-        # print(self.A_koop)
-        
         G = lstsq(omega.T,Y.T,rcond=None)[0].T
         
         self.A_koop = G[: , :self.Y_plus.shape[-1]] 
         self.B_koop = G[: , self.Y_plus.shape[-1]:]
-        # print(self.A_koop)
 
-        # print(np.linalg.eig(_Atilde))
+        print(self.B_koop)
 
-        # print(_Atilde)
-        # print(_Btilde)
+    def generate_hybrid_model(self):
 
-        # print(_B)
+
+        U = np.concatenate((self.X_minus.reshape(-1, self.X_minus.shape[-1]),
+                            self.U_minus.reshape(-1, self.U_minus.shape[-1])),axis=1).T
+
+        Y_temp = self.Eta_minus.reshape(-1, self.Eta_minus.shape[-1]).T
+
+        Y = Y_temp[0,:] + Y_temp[1,:]
+        Y = Y.T
+        Y = np.expand_dims(Y, axis=0)
+
+
+        # (_,A_eta_hybrid_disc,_,_,_) = cont2discrete((self.plant.A_x,self.plant.A_eta_hybrid,np.array([0.0,0.0]),np.array([0.0])),self.dt_data)
+        
+        (A_x_disc,_,_,_,_) = cont2discrete((self.plant.A_x,self.plant.B_x,np.array([0.0,0.0]),np.array([0.0])),self.dt_data)
+        B_x_disc = self.plant.B_x*self.dt_data
+        A_eta_hybrid_disc = self.plant.A_eta_hybrid*self.dt_data
+        
+        print('discrete matrices')
+        print(A_x_disc)
+        print(B_x_disc)
+        print(A_eta_hybrid_disc)
+       
+        NumURows = 200
+        NumUCols = 1500
+        # print('N4SID inputs')
+        # print(U.shape)
+        # print(Y.shape)
+
+        A_til,B_til,C_til,D_til,_,S = ssid.N4SID(U,Y,NumURows,NumUCols,2)
+        
+        # plt.plot(S / S.sum())
+        # plt.show()
+        print('n4sid matrices')
+        print(A_til)
+        print(B_til)
+        print(C_til)
+        print(D_til)
+
+
+        B_til_1 = B_til[:,:self.plant.N_x]
+        B_til_2 = B_til[:,self.plant.N_x:]
+        D_til_1 = D_til[:,:self.plant.N_x]
+        D_til_2 = D_til[:,self.plant.N_x:]
+
+        A1 = A_x_disc + A_eta_hybrid_disc.dot(D_til_1)
+        A2 = A_eta_hybrid_disc.dot(C_til)
+        B1 = B_x_disc + A_eta_hybrid_disc.dot(D_til_2)
+
+
+        print('multiplied matrices')
+        print(A_eta_hybrid_disc.dot(D_til_1))
+        self.A_hybrid =  np.block([[A1     ,  A2],
+                                   [B_til_1,  A_til ]])
+
+        print(eigvals(A_til))
+
+        self.B_hybrid = np.block([[B1],
+                                  [B_til_2]])
+
+        # print(self.A_hybrid)
+        # print(self.B_hybrid.shape)
 
     def generate_N4SID_model(self):
 
         U = self.U_minus.reshape(-1, self.U_minus.shape[-1]).T
         Y = self.Y_plus.reshape(-1, self.Y_plus.shape[-1]).T
         
-        NumURows = 10
-        NumUCols = 5600
+        # print(U.shape,Y.shape)
+
+        NumURows = 30
+        NumUCols = 1800
         
         print('u',U.shape)
         print('y',Y.shape)
         print('NumURows',NumURows)
         print('NumUCols',NumUCols)
 
-
-
         AID,BID,CID,DID,CovID,S = ssid.N4SID(U,Y,NumURows,NumUCols,4)
         print(AID,BID,CID,DID)
         plt.plot(S / S.sum())
         plt.show()
 
-    def simulate_system_nonlinear(self, x_0, u_func, t_f):
-        '''
-        Simulate the full nonlinear system
-        Arguments:
-        x_0: initial state
-        u_func: control function
-        t_f: final time
-
-        '''
-        t_0 = 0.0
-
-        #initialize the ode integrator
-        r = ode(self.plant.f).set_integrator('vode', method='bdf')
-        r.set_initial_value(x_0,t_0)
-
-        t_array = []
-        x_array = []
-        u_array = []
-        eta_array = []
-
-        # initial state and 
-        x_t = copy.copy(x_0)
-        u_t = u_func(self.plant.g(r.t,x_0,0.0),r.t)
-
-        #simulate the system
-        while r.successful() and r.t < t_f:
-            
-            x_t_minus = copy.copy(x_t)
-            u_t_minus = copy.copy(u_t)
-
-            #define the control input. A random value is used for data generation
-            u_t = u_func(self.plant.g(r.t,x_t_minus,u_t_minus),r.t)
-
-            r.set_f_params(u_t).set_jac_params(u_t)
-            x_t = r.integrate(r.t+self.dt_data)
-            
-            t_array.append(r.t)
-            x_array.append(x_t)
-            u_array.append([u_t])
-
-            # eta_array.append(self.plant.phi(r.t,x_t,u_t))
-
-        # eta_dot_array = savgol_filter(np.array(eta_array),
-        #                               window_length = 5, polyorder = 3,
-        #                               deriv = 1, axis=0)
-
-        return np.array(t_array), np.array(u_array), np.array(x_array)
-    
     def f_dfl(self,t,xi,u):
 
         u = np.array([u])
         x   = xi[:self.plant.N_x]
         eta = xi[self.plant.N_x:self.plant.N_x + self.plant.N_eta]
+        
+
         x_dot   = np.dot(self.plant.A_x,x) +  np.dot(self.plant.A_eta, eta) + np.dot(self.plant.B_x,u) # temporary fix.
         eta_dot = np.dot(self.H_x,x) +  np.dot(self.H_eta, eta) + np.dot(self.H_u,u)
 
         return np.concatenate((x_dot,eta_dot))
 
     def f_koop(self,t,x,u):
-        
+
         u = np.array([u])
         y_dot = np.dot(self.A_koop,x) + np.dot(self.B_koop,u)
 
         return y_dot
 
-    def simulate_system_dfl(self, x_0, u_func, t_f):
+    def f_hybrid(self,t,x,u):
+
+        u = np.array([u])
+        x_dot = np.dot(self.A_hybrid,x) + np.dot(self.B_hybrid,u)
+
+        return x_dot
+
+    @staticmethod
+    def simulate_system(x_0, u_minus, t_f, dt, u_func, f_func, g_func, continuous = True):
         '''
-        Simulate the full nonlinear system
+        Simulate a system in continuous time
         Arguments:
         x_0: initial state
         u_func: control function
         t_f: final time
 
         '''
-        t_0 = 0.0
-        # initial state and 
-        u_0 = u_func(self.plant.g(t_0,x_0,0.0),t_0)
+        # initial time and input
+        t = 0.0
 
-        #initialize the ode integrator
-        eta_0 = self.plant.phi(t_0,x_0,u_0)
+        # create numerical integration object
+        if continuous:
+            r = ode(f_func).set_integrator('vode', method = 'bdf')
+            r.set_initial_value(x_0,t)
+
+        t_array = []
+        x_array = []
+        u_array = []
+        y_array = []
+        
+        # initial state and 
+        x_t = copy.copy(x_0)
+        y_t = g_func(t, x_t, u_minus)
+        u_t = u_func(y_t, t)
+
+        t_array.append(t)
+        x_array.append(x_t)
+        u_array.append([u_t])
+        y_array.append(g_func(t,x_t,u_minus))
+
+
+        #Simulate the system
+        while t < t_f:
+
+            if continuous:
+                r.set_f_params(u_t).set_jac_params(u_t)
+                x_t = r.integrate(r.t + dt)
+            else:
+                x_t = f_func(t, x_t, u_t)
+
+            t = t + dt
+            y_t = g_func(t, x_t, u_t)
+            u_t = u_func(g_func(t, x_t, u_t), t)
+
+            t_array.append(t)
+            x_array.append(x_t)
+            u_array.append([u_t])
+            y_array.append(y_t)
+
+        return np.array(t_array), np.array(u_array), np.array(x_array), np.array(y_array)
+
+    def simulate_system_nonlinear(self, x_0, u_func, t_f):
+
+        u_minus = np.zeros((self.plant.N_u,1))
+        t,u,x,y = self.simulate_system(x_0, u_minus, t_f, self.dt_data,
+                                        u_func, self.plant.f, self.plant.g,
+                                        continuous = True)
+        
+        return t, u, x, y
+        
+    def simulate_system_dfl(self, x_0, u_func, t_f):
+
+        u_minus = np.zeros((self.plant.N_u,1))
+        eta_0 = self.plant.phi(0.0, x_0, u_minus)
         xi_0 = np.concatenate((x_0,eta_0))
 
-        r = ode(self.f_dfl).set_integrator('vode', method='bdf')
-        r.set_initial_value(xi_0,t_0)
-
-        # print(dir(r))
-        # print(r.y)
-        t_array = []
-        xi_array = []
-        u_array = []
-        eta_array = []
+        t,u,xi,y = self.simulate_system(xi_0, u_minus, t_f, self.dt_data,
+                                                   u_func, self.f_dfl, self.plant.g,
+                                                   continuous = True)
         
-        # initial state and 
-        x_t = copy.copy(x_0)
-        u_t = 0.0
-        xi_t = copy.copy(xi_0)
-        #simulate the system
-        while r.successful() and r.t < t_f:
-            
-            x_t_minus = copy.copy(xi_t[:self.plant.N_x])
-            u_t_minus = copy.copy(u_t)
-
-            #define the control input. A random value is used for data generation
-            u_t = u_func(self.plant.g(r.t,x_t_minus,u_t_minus),r.t)
-
-            r.set_f_params(u_t).set_jac_params(u_t)
-
-            xi_t = r.integrate(r.t+self.dt_data)
-
-            t_array.append(r.t)
-            xi_array.append(xi_t)
-            u_array.append([u_t])
-
-            # eta_array.append(self.plant.phi(r.t,x_t,u_t))
-
-        # eta_dot_array = savgol_filter(np.array(eta_array),
-        #                               window_length = 5, polyorder = 3,
-        #                               deriv = 1, axis=0)
-
-        return np.array(t_array), np.array(u_array), np.array(xi_array)
+        return t, u, xi, y 
 
     def simulate_system_koop(self, x_0, u_func, t_f):
-        '''
-        Simulate the full nonlinear system
-        Arguments:
-        x_0: initial state
-        u_func: control function
-        t_f: final time
 
-        '''
-        t_0 = 0.0
-        # initial state and 
-        u_0 = u_func(self.plant.g(t_0,x_0,0.0),t_0)
+        u_minus = np.zeros((self.plant.N_u,1))
+        xi_0 = self.plant.g(0.0, x_0, u_minus)
 
-        #initialize the ode integrator
-        xi_0 = self.plant.g(t_0,x_0,u_0)
+        t,u,xi,y = self.simulate_system(xi_0, u_minus, t_f, self.dt_data,
+                                        u_func, self.f_koop, self.plant.g,
+                                        continuous = False)
+        return t, u, xi, y 
 
-        t_array = []
-        xi_array = []
-        u_array = []
-        eta_array = []
-        
-        # initial state and 
-        x_t = copy.copy(x_0)
-        u_t = 0.0
-        xi_t = copy.copy(xi_0)
-        #simulate the system
-        t=0.0
+    def simulate_system_hybrid(self, x_0, u_func, t_f):
 
-        while True and t < t_f:
-            
-            t = t + self.dt_data
-            xi_t_minus = copy.copy(xi_t)
-            u_t_minus = copy.copy(u_t)
+        u_minus = np.zeros((self.plant.N_u,1))
+        eta_0 = self.plant.phi(0.0, x_0, u_minus)
+        xi_0 = np.concatenate((x_0,eta_0))
+        # print(xi_0)
+        t,u,xi,y = self.simulate_system(xi_0, u_minus, t_f, self.dt_data,
+                                                   u_func, self.f_hybrid, self.plant.g,
+                                                   continuous = False)
 
-            #define the control input. A random value is used for data generation
-            u_t = u_func(self.plant.g(t,xi_t_minus[:self.plant.N_x],u_t_minus),t)
-
-            xi_t = self.f_koop(t, xi_t_minus, u_t)
-            t_array.append(t)
-            xi_array.append(xi_t)
-            u_array.append([u_t])
-
-        return np.array(t_array), np.array(u_array), np.array(xi_array)
+        return t,u,xi,y
--- src/example_simple.py
+++ src/example_simple.py
@@ -14,12 +14,10 @@ import matplotlib.pyplot as plt
 
 m = 1.0
 k11 = 0.2
-
 k13 = 2.0
-k21 = 1.0
-k23 = 1.2
-b1  = 1.0
-b2  = 1.5
+
+b1  = 3.0
+
 
 class Plant1(DFLDynamicPlant):
     
@@ -30,7 +28,7 @@ class Plant1(DFLDynamicPlant):
         self.N_x = 2
         self.N_eta = 2
         self.N_u = 1
-        self.N_y =4
+        self.N_y = 4
 
         self.N = self.N_x + self.N_eta
 
@@ -45,36 +43,62 @@ class Plant1(DFLDynamicPlant):
         self.x_init_min = np.array([-2.0,-2.0])
         self.x_init_max = np.array([2.0 ,2.0])
 
-
+        ##### hybrid model
+        self.N_eta_hybrid = 1
+        self.A_eta_hybrid = np.array([[0.0],
+                                     [-1/m]])
 
 
     # functions defining constituitive relations for this particular system
-    def phi_c1(self,q):
+    @staticmethod
+    def phi_c1(q):
         e = k11*q + k13*q**3
         return e
 
-    def phi_r1(self,f):
-        e = np.sign(f)*np.abs(f)*np.abs(f)
+    @staticmethod
+    def phi_r1(f):
+        # e = b1*np.sign(f)*np.abs(f)*np.abs(f)
+        e = b1*f**3
         return e
 
-    def phi_rc(self,q,v):
+    @staticmethod
+    def phi_rc(q,v):
         return 5*v*np.abs(q)
+    
     # nonlinear state equations
     def f(self,t,x,u):
 
         x_dot = np.zeros(x.shape)
         q,v = x[0],x[1]
         x_dot[0] = v
-        x_dot[1] = -self.phi_r1(v) - self.phi_c1(q) + u #-self.phi_c1(q) - self.phi_r1(v) + u
+        x_dot[1] = -self.phi_r1(v) -self.phi_c1(q) + u 
         return x_dot
 
     # nonlinear observation equations
-    def g(self,t,x,u):
+    @staticmethod
+    def g(t,x,u):
         q,v = x[0],x[1]
         y = np.array([q,v])
-        # y = np.array([q,v,self.phi_c1(q), self.phi_r1(v)])
         return y 
     
+    @staticmethod
+    def gkoop1(t,x,u):
+        q,v = x[0],x[1]
+        y = np.array([q,v,Plant1.phi_c1(q), Plant1.phi_r1(v)])
+        return y  
+    
+    @staticmethod
+    def gkoop2(t,x,u):
+        q,v = x[0],x[1]
+
+        y = np.array([q,v,q**2,q**3,q**4,q**5,q**6,q**7,
+                      v**2,v**3,v**4,v**5,v**6,v**7,v**9,v**11,v**13,v**15,v**17,v**19,
+                      v*q,v*q**2,v*q**3,v*q**4,v*q**5,
+                      v**2*q,v**2*q**2,v**2*q**3,v**2*q**4,v**2
+                      *q**5,
+                      v**3*q,v**3*q**2,v**3*q**3,v**3*q**4,v**3*q**5])
+        return y 
+
 
     # auxiliary variables (outputs from nonlinear elements)
     def phi(self,t,x,u):
@@ -82,47 +106,77 @@ class Plant1(DFLDynamicPlant):
         outputs the values of the auxiliary variables
         '''
         q,v = x[0],x[1]
+        
         eta = np.zeros(self.N_eta)
-        # eta[0] = self.phi_c1(q) + self.phi_r1(v)
-        # eta[1] = 0.0
         eta[0] = self.phi_c1(q)
         eta[1] = self.phi_r1(v)
-        # eta[2] = self.phi_c1(q)*q
-        # eta[3] = self.phi_r1(v)*v
+
         return eta
 
+    def phi_hybrid(self,t,x,u):
+        '''
+        outputs the values of the auxiliary variables
+        '''
+        q,v = x[0],x[1]
+        eta = np.zeros(self.N_eta)
+        eta[0] = self.phi_c1(q) + self.phi_r1(v)
+
+        return eta
+
+###########################################################################################
+
+#Dummy forcing laws
 def zero_u_func(y,t):
-    return 0 
+    return 1 
 
 def rand_u_func(y,t):
-    return np.random.normal(0.0,0.3) 
+    return np.sign(np.random.normal(0.0,0.3))
 
 def sin_u_func(y,t):
     return np.sin(3*t) 
 
-def g1(t,x,u):
-    q,v = x[0],x[1]
 
-    # y = np.array([q,v,plant2.phi_c1(q)+plant2.phi_r1(v)])
-    print(-1/v)
-    print(v,np.exp(-1/v))
-    y = np.array([q,v,q**2,q**3,q**4,q**5,q**6,q**7,
-                  v**2,v**3,v**4,v**5,v**6,v**7,v**9,v**11,v**13,v**15,v**17,v**19,
-                  v*q,v*q**2,v*q**3,v*q**4,v*q**5,
-                  v**2*q,v**2*q**2,v**2*q**3,v**2*q**4,v**2
-                  *q**5,
-                  v**3*q,v**3*q**2,v**3*q**3,v**3*q**4,v**3*q**5])
-    return y 
+
+
 
 if __name__== "__main__":
     plant1 = Plant1()
-    plant2 = Plant1()
-    setattr(plant2, "g", g1)
 
     dfl1 = DFL(plant1)
+    setattr(plant1, "g", Plant1.gkoop2)
+
     dfl1.generate_data_from_random_trajectories()
-    dfl1.generate_N4SID_model()
+    # dfl1.generate_H_matrix()
+    dfl1.generate_hybrid_model()
+    # dfl1.generate_N4SID_model()
+    # dfl1.generate_K_matrix()
+
+    # x_0 = np.random.uniform(plant1.x_init_min,plant1.x_init_max)
+    x_0 = np.array([0,0])
+    t, u, x_nonlin, y_nonlin = dfl1.simulate_system_nonlinear(x_0, sin_u_func, 10.0)
+    # t, u, x_dfl, y_dfl = dfl1.simulate_system_dfl(x_0, zero_u_func, 10.0)
+    # t, u, x_koop1, y_koop = dfl1.simulate_system_koop(x_0, zero_u_func, 10.0)
+    t, u, x_hybrid, y_hybrid = dfl1.simulate_system_hybrid(x_0, sin_u_func, 10.0)
+
+    # dfl1.generate_N4SID_model()
+
+    fig, axs = plt.subplots(2, 1)
+
+    axs[0].plot(t, x_nonlin[:,0], 'b')
+    # axs[0].plot(t, x_dfl[:,0] ,'b-.')
+    # axs[0].plot(t, x_koop1[:,0] ,'b.')
+    axs[0].plot(t, x_hybrid[:,0] ,'b--')
+
+    axs[1].plot(t, x_nonlin[:,1],'r')
+    # axs[1].plot(t, x_dfl[:,1],'r-.')
+    # axs[1].plot(t, x_koop1[:,1] ,'r.')
+    axs[1].plot(t, x_hybrid[:,1] ,'r--')
+
+    plt.show()
+
     exit()
+
+    ##################################################################
     dfl1.generate_H_matrix()
     dfl1.generate_K_matrix()
 
